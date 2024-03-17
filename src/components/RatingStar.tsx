f01cd69fda6531985345989a598b20d16f148d84
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-03-17 17:55:32 +0100

    Update package.json and RatingStar component

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -3,7 +3,7 @@ import Star from './Star'
 // Types
 import type { RatingStarProps } from './types/types'
 // Styles
-import './styles/react-ts-rating-star.scss'
+import './react-ts-rating-star.scss'
 
 /**
  * RatingStar component for displaying a rating star system.
